f18cc4d6e9637950af4454963bff1a225086b148
garbeam <garbeam>
2004-03-29 13:40:44 +0000

    trashig

---
+++ kobold/docs/trash/Trash1.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 29.03.2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package docs.trash;
+
+/**
+ * @author garbeam
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Trash1 {
+
+	private Trash2 trash2;
+
+	public Trash2 getTrash2() {
+		return trash2;
+	}
+
+	public void setTrash2(Trash2 trash2) {
+		this.trash2 = trash2;
+	}
+
+}
