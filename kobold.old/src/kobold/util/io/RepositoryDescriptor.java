0add77ee50f07616a43c75a8a057a5d7a2bee279
vanto <vanto>
2004-03-31 00:04:58 +0000

    refactoring

---
+++ kobold.old/src/kobold/util/io/RepositoryDescriptor.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2004 Armin Cont, Anselm R. Garbe, Bettina Druckenmueller,
+ *                    Martin Plies, Michael Grosse, Necati Aydin,
+ *                    Oliver Rendgen, Patrick Schneider, Tammo van Lessen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * $Id: RepositoryDescriptor.java,v 1.1 2004/03/31 00:04:59 vanto Exp $
+ */
+
+package kobold.util.io;
+
+/**
+ * Base class for locating repositories.
+ * This descriptor is used for repositories (whatever type).
+ * It provides host, protocol and file description.
+ *
+ * @author garbeam
+ */
+public class RepositoryDescriptor {
+
+    public RepositoryDescriptor() {
+
+    }
+}
