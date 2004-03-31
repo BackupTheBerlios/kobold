5b2e1d6149a3380a3be3e87d9db457eafed1b2bf
vanto <vanto>
2004-03-31 00:08:13 +0000

    tests

---
+++ kobold.old/tests-src/kobold/TestSuite.java
@@ -0,0 +1,17 @@
+/*
+ * kobold
+ * TODO
+ * Created on 31.03.2004
+ *
+ */
+package kobold;
+
+/**
+ * TestSuite
+ * 
+ * @author Tammo van Lessen
+ * @version $Id: TestSuite.java,v 1.1 2004/03/31 00:08:13 vanto Exp $
+ */
+public class TestSuite {
+
+}
