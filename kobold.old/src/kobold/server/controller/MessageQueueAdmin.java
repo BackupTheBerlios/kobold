d09f2f39f73e7f234f07884a7a037eb5643a43bf
neccaino <neccaino>
2004-04-08 10:29:06 +0000

    added

---
+++ kobold.old/src/kobold/server/controller/MessageQueueAdmin.java
@@ -0,0 +1,26 @@
+package kobold.server.controller;
+
+import java.lang.String;
+
+/**
+ * this class admins the server-side message queues for all users
+ *
+ * @author Armin Cont
+ */
+public class MessageQueueAdmin {
+
+/**
+ * adds a message to the message queue of the user given by msg
+ *
+ * @param msg the message to add
+ */
+public void addMessage(KoboldMessage msg) {
+  }
+
+/**
+ * @return the given user's server-side message queue
+ */
+public MessageQueue getMessageQueue(String username) {
+  return null;
+  }
+}
