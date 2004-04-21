2e3d476f315d9e99561081a4d086b82d1bfb48f3
grosseml <grosseml>
2004-04-21 15:15:40 +0000

    Historien Klasse zur Nachverfolgung des Workflow Baums

---
+++ kobold/src/kobold.common/src/kobold/common/data/History.java
@@ -0,0 +1,127 @@
+/*
+ * Created on Apr 21, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package kobold.common.data;
+
+import java.util.HashMap;
+
+/**
+ * @author grosseml
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class History {
+	private int id;
+	private String name;
+	private String performer;
+	private String decision_made;
+	private String decision_opportunities;
+	private String choices;
+	private int Step;
+	
+	
+	
+
+	/**
+	 * @return
+	 */
+	public String getChoices() {
+		return choices;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getDecision_made() {
+		return decision_made;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getDecision_opportunities() {
+		return decision_opportunities;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getPerformer() {
+		return performer;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getStep() {
+		return Step;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setChoices(String string) {
+		choices = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setDecision_made(String string) {
+		decision_made = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setDecision_opportunities(String string) {
+		decision_opportunities = string;
+	}
+
+	/**
+	 * @param i
+	 */
+	public void setId(int i) {
+		id = i;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setName(String string) {
+		name = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setPerformer(String string) {
+		performer = string;
+	}
+
+	/**
+	 * @param i
+	 */
+	public void setStep(int i) {
+		Step = i;
+	}
+
+}
