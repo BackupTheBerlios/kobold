7d43f344fc31a0bda91ae9ad320c7a390ed61bbb
memyselfandi <memyselfandi>
2004-05-10 16:31:15 +0000

    test cvs

--- kobold.old/tests-src/kobold/TestSuite.java
+++ kobold.old/tests-src/kobold/TestSuite.java
@@ -10,8 +10,9 @@ package kobold;
  * TestSuite
  * 
  * @author Tammo van Lessen
- * @version $Id: TestSuite.java,v 1.1 2004/03/31 00:08:13 vanto Exp $
+ * @version $Id: TestSuite.java,v 1.2 2004/05/10 16:31:15 memyselfandi Exp $
  */
 public class TestSuite {
+	this is a test
 
 }
