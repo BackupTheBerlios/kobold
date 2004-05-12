22bc43aae088183be4816e4a5dfd0bf9d904675e
rendgeor <rendgeor>
2004-05-12 17:49:58 +0000

    changed DroolsTest

---
+++ kobold/src/kobold.server/tests-src/DroolsManagerTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2003 - 2004 Necati Aydin, Armin Cont, 
+ * Bettina Druckenmueller, Anselm Garbe, Michael Grosse, 
+ * Tammo van Lessen,  Martin Plies, Oliver Rendgen, Patrick Schneider
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * $Id: DroolsManagerTest.java,v 1.1 2004/05/12 17:49:58 rendgeor Exp $
+ *
+ */
+
+import junit.framework.TestCase;
+
+import kobold.server.workflow.*;
+import kobold.common.data.*;
+
+/**
+	 * @author garbeam
+	 *
+	 * TestCase for DroolsManager.
+	 */
+	
+public class DroolsManagerTest extends TestCase {
+
+	/**
+	 * Constructor for DroolsManagerTest.
+	 * @param arg0
+	 */
+	
+
+	
+	public DroolsManagerTest(String arg0) {
+		
+		super(arg0);
+	}
+	
+	public void testSerialize() {
+
+		WorkflowMessage msg = new WorkflowMessage();
+		WorkflowMessage msg2 = new WorkflowMessage();
+		msg.setType("Core Group Suggestion");
+		msg.setSender("Sender");
+		WorkflowEngine.applWorkflow(msg);
+		System.out.println("");
+		
+		System.out.println("Phase 1");
+		System.out.println("Workflow-Typ: " + msg.getType());
+		System.out.println("XML: " + msg.getMessageText());
+		System.out.println("Empfänger: " + msg.getReciever());
+		System.out.println("");
+		
+		msg.setSender("Sender2");
+		WorkflowEngine.applWorkflow(msg);
+		
+		System.out.println("Phase 2");
+		System.out.println("Workflow-Typ: " + msg.getType());
+		System.out.println("XML: " + msg.getMessageText());
+		System.out.println("Empfänger: " + msg.getReciever());
+		System.out.println("");
+		
+		msg.setSender("Sender3");
+		History his3 = new History();
+		WorkflowEngine.applWorkflow(msg);
+		
+		System.out.println("Phase 3");
+		System.out.println("Workflow-Typ: " + msg.getType());
+		System.out.println("XML: " + msg.getMessageText());
+		System.out.println("Empfänger: " + msg.getReciever());
+		his3 = (History) msg.getHistoryList().get(msg.getHistoryList().size() - 1);
+		System.out.println("");
+		
+		
+	}
+}
