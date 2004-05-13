60b923e4aab5639c69556155ca45cbe06154147d
memyselfandi <memyselfandi>
2004-05-13 16:33:39 +0000

    first try to comply with our QS Diktator ;)

--- kobold/src/kobold.client.vcm/src/kobold/client/vcm/controller/KoboldRepositoryOperations.java
+++ kobold/src/kobold.client.vcm/src/kobold/client/vcm/controller/KoboldRepositoryOperations.java
@@ -1,9 +1,29 @@
 /*
- * Created on 12.05.2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Copyright (c) 2004 Armin Cont, Anselm R. Garbe, Bettina Druckenmueller,
+ *                    Martin Plies, Michael Grosse, Necati Aydin,
+ *                    Oliver Rendgen, Patrick Schneider, Tammo van Lessen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * 
  */
+
 package kobold.client.vcm.controller;
 
 import org.eclipse.core.resources.IResource;
@@ -13,9 +33,7 @@ import org.eclipse.team.internal.core.simpleAccess.SimpleAccessOperations;
 
 /**
  * @author schneipk
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ ** @ TODO shitty and not needed interface
  */
 public interface KoboldRepositoryOperations extends SimpleAccessOperations {
 	
--- kobold/src/kobold.client.vcm/src/kobold/client/vcm/popup/action/DisconnectAction.java
+++ kobold/src/kobold.client.vcm/src/kobold/client/vcm/popup/action/DisconnectAction.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2004 Armin Cont, Anselm R. Garbe, Bettina Druckenmueller,
+ *                    Martin Plies, Michael Grosse, Necati Aydin,
+ *                    Oliver Rendgen, Patrick Schneider, Tammo van Lessen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
 package kobold.client.vcm.popup.action;
 
 import org.eclipse.core.resources.IProject;
@@ -8,14 +33,22 @@ import org.eclipse.team.core.TeamException;
 import org.eclipse.team.internal.ui.actions.TeamAction;
 
 /**
+ * This class is triggerd for the disconnection of a projekt 
+ * from the Kobold Repository Provider (WrapperProvider)
  */
 public class DisconnectAction extends TeamAction{
 	
 	/**
+	 * This is the standard Constructor for the Class
+	 * in this casse nothing needs to be done
 	 */
 	public DisconnectAction() {
 
 	}
+	
+	/**
+	 * @see org.eclipse.ui.IActionDelegate#run(IAction)
+	 */	
 	public void run(IAction action) {
 		IProject projects[] = getSelectedProjects();
 		try {
@@ -30,7 +63,6 @@ public class DisconnectAction extends TeamAction{
 	 * @see org.eclipse.team.internal.ui.actions.TeamAction#isEnabled()
 	 */
 	protected boolean isEnabled() throws TeamException {
-		// TODO Auto-generated method stub
 		return true;
 	}
 }
