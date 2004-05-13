4c6d0af95deb7af8c84b8a966153aa1fa17f3bdf
martinplies <martinplies>
2004-05-13 14:38:34 +0000

    *** empty log message ***

---
+++ kobold/src/kobold.common/src/WorkflowItem.java
@@ -0,0 +1,20 @@
+/*
+ * Created on May 13, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+/**
+ * @author pliesmn
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class WorkflowItem {
+	public final String radio = "radio"; 
+	public final String check = "check";
+	
+	String description;
+	String name;
+	String type;
+}
