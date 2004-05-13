15c37a7ba169db2adaa6a2ff61bd5de68d751857
grosseml <grosseml>
2004-05-13 14:07:10 +0000

    neues Design von Tammo

--- kobold/src/kobold.common/src/kobold/common/data/WorkflowMessage.java
+++ kobold/src/kobold.common/src/kobold/common/data/WorkflowMessage.java
@@ -13,19 +13,106 @@ import java.util.*;
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 public class WorkflowMessage extends KoboldMessage {
-   private HashMap result;
-   private String  xmlMessage;
-   private List historyList  = new ArrayList();
-   private String type;
-   
-   
-	  public void setType(String str) {
-		  type = str;
-	  }
-   
-	  public String getType() {
-		  return type;
-	  }
+	private int workflowID;
+	private string comment;
+	private List history;
+	private List items;
+	private int step;
+
+	public void addHistoryObject(WorkflowMessage newWflowMessage){
+			history.add(newWflowMessage);
+	}
+	
+	public void addItem(WorkflowItem nWflowItem){
+		items.add(nWflowItem);
+	}
+	
+	public WorkflowMessage(int wfid, string ncomm, int nstep, List nhist, List nitems){
+		workflowID = wfid;
+		comment = ncomm;
+		step = nstep;
+		history = nhist;
+		items = nitems;
+		
+	}
+	
+	public WorkflowMessage(){
+		workflowID = 0;
+		comment = "";
+		step = 0;
+		history = new ArrayList();
+		items = new ArrayList();		
+	}
+
+	/**
+	 * @return
+	 */
+	public string getComment() {
+		return comment;
+	}
+
+	/**
+	 * @return
+	 */
+	public List getHistory() {
+		return history;
+	}
+
+	/**
+	 * @return
+	 */
+	public List getItems() {
+		return items;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getStep() {
+		return step;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getWorkflowID() {
+		return workflowID;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setComment(string string) {
+		comment = string;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setHistory(List list) {
+		history = list;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setItems(List list) {
+		items = list;
+	}
+
+	/**
+	 * @param i
+	 */
+	public void setStep(int i) {
+		step = i;
+	}
+
+	/**
+	 * @param i
+	 */
+	public void setWorkflowID(int i) {
+		workflowID = i;
+	}
 
 }   
    
