9fee68d19c45f9133b22b66b6973e628d52f1533
martinplies <martinplies>
2004-05-14 10:09:23 +0000

    *** empty log message ***

--- kobold/src/kobold.client.plam/src/kobold/client/plam/workflow/WorkflowDialog.java
+++ kobold/src/kobold.client.plam/src/kobold/client/plam/workflow/WorkflowDialog.java
@@ -13,53 +13,82 @@ package kobold.client.plam.workflow;
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 
+import java.util.ListIterator;
+
+import org.eclipse.core.internal.resources.Workspace;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.*;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Button;
+import kobold.common.data.*;
 /**
  * @author scheglov_ke
  */
 public class WorkflowDialog extends Dialog {
-	Button cb;
+	WorkflowItemGroup workflowItemGroup;
+	String description;
+	String subject;
+	Composite parent1;
+	//List checkButton
+
 	
-	public WorkflowDialog(Shell parentShell) {
+	public WorkflowDialog(Shell parentShell, WorkflowItemGroup  workflowItemGroup, String subject, String description) {		
 		super(parentShell);
+		this.parent1 = parentShell;
+		this.workflowItemGroup = workflowItemGroup;
+		this.subject = subject;
+		this.description = description;	
+			
 	}
 	
 	protected Control createDialogArea(Composite parent) {
 		Composite area = (Composite) super.createDialogArea(parent);
-		area.setLayout(new FillLayout());
-		{
-			final TabFolder tabFolder = new TabFolder(area, SWT.NONE);
-			{
-				final TabItem tabItem = new TabItem(tabFolder, SWT.NONE);
-				tabItem.setText("tab 1");
-				{
-					final Button button = new Button(tabFolder, SWT.CHECK);
-					tabItem.setControl(button);
-					button.setText("button 1");
-				}
-			}
-			{
-				final TabItem tabItem = new TabItem(tabFolder, SWT.NONE);
-				tabItem.setText("tab 2");
-				{
-					final Button button = new Button(tabFolder, SWT.NONE);
-					tabItem.setControl(button);
-					button.setText("button 2");
-				}
-			}
-		}
+		area.setLayout(new GridLayout());
+		Label lbl = new Label(area,SWT.NONE);
+		lbl.setText(subject);
+		Button b = new Button(parent1,SWT.PUSH);
+		b.setText("dsgtdfgtfh");
+		//addItem(area,workflowItemGroup);
+		
 		// DESIGNER: Add controls before this line.
 		return area;
+		
 	}
+	/**
+	 * @param area
+	 * @param workflowItemGroup2
+	 */
+	private void addItem(Composite area, WorkflowItemGroup workflowItemGroup) {		
+		Composite comp = new Composite(area,SWT.NONE);
+		for (ListIterator ite = workflowItemGroup.getWorkflowItems().listIterator();
+		     ite.hasNext();){
+			Object object = ite.next();
+			if (object instanceof WorkflowItemGroup){
+			    addItem(comp,(WorkflowItemGroup) object);
+			} else if(object instanceof WorkflowItem){
+			   addItem(comp,(WorkflowItem) object);
+			}					
+		}
+	}
+	
+	private void addItem(Composite area, WorkflowItem workflowItem) {
+	   int style;
+		if (workflowItem.getType() == (WorkflowItem.RADIO))
+		  style = SWT.RADIO;
+		else style = SWT.CHECK;			
+		Button b = new Button(area, style);
+			
+					
+	}
+	
+
 	protected void createButtonsForButtonBar(Composite parent) {
 		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
 		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
@@ -68,6 +97,9 @@ public class WorkflowDialog extends Dialog {
 		super.configureShell(newShell);
 		newShell.setText("Workflow Dialog");
 	}
+	
+	
+	
 	
 /*	public boolean getCBValue(){
 		cb.g		
---
+++ kobold/src/kobold.client.plam/src/kobold/client/plam/workflow/WorkflowViewItemFactory.java
@@ -0,0 +1,34 @@
+/*
+ * Created on May 13, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package kobold.client.plam.workflow;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.eclipse.swt.widgets.Composite;
+
+import kobold.common.data.WorkflowItemGroup;
+
+/**
+ * @author pliesmn
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class WorkflowViewItemFactory {
+	List items = new ArrayList();  
+	
+	private WorkflowViewItemFactory(){
+		
+	}
+	
+//	public static WorkflowViewItemFactory createWorkflowItems(Composite swtGroup, WorkflowItemGroup itemGroup) {
+//
+//		
+//		
+//	}
+}
