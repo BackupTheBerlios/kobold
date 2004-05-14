53e6d1def14bed0a4d463e09b0c001f0fa2c6fb1
martinplies <martinplies>
2004-05-14 09:28:28 +0000

    *** empty log message ***

---
+++ kobold/src/kobold.common/src/kobold/common/data/WorkflowItem.java
@@ -0,0 +1,45 @@
+package kobold.common.data;
+/*
+ * Created on May 13, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+/**
+ * @author pliesmn
+ *
+ * A WorkflowItem is a Checkbutton, a Radiobutton or a  single-Line-TextWindow
+ * The data, to display a WorkFlowItem is stored here.  
+ * 
+ */
+public class WorkflowItem {
+	public final static int TEXT = 0; 
+	public final static int CHECK = 1;
+	
+	String description;
+	String nameValue; // The Name of the Checkbutton or Textwindow or the value of the Radiobutton 
+	int type;
+	
+	public WorkflowItem(String nameValue,String description, int type ) {
+		this.nameValue = nameValue;
+		this.description = description;
+		this.type = type;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public String getName(){
+		return nameValue;
+	}
+	
+	public String getValue(){
+		return nameValue;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+}
---
+++ kobold/src/kobold.common/src/kobold/common/data/WorkflowItemRadioGroup.java
@@ -0,0 +1,26 @@
+/*
+ * Created on May 14, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package kobold.common.data;
+
+/**
+ * @author pliesmn
+ *
+ * The WorkflowRadioGroup contains the Radiobuttons, that should be 
+ * displayed together. It can also contain the other WorkflowItems. 
+ * Additional to the WorkflowItemGroup the WorkflowItemRadioGroup
+ * contains the name, that are needed to store the value of the 
+ * selected Radiobutton.
+ * 
+ */
+public class WorkflowItemRadioGroup extends WorkflowItemGroup {
+	private String name;	
+	
+	public WorkflowItemRadioGroup(String name){
+	  	this.name = name;
+	}
+		
+}
