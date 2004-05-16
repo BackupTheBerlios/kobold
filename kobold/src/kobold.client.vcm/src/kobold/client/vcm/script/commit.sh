63b34705df295d597ffbe3aa1365f42efea95f09
rendgeor <rendgeor>
2004-05-16 16:17:52 +0000

    changed commit

--- kobold/src/kobold.client.vcm/src/kobold/client/vcm/script/commit.sh
+++ kobold/src/kobold.client.vcm/src/kobold/client/vcm/script/commit.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 #PARAMETER:
+#$1 working directory: /tmp
+
+cd $1
+
 #-d CVS_root_directory
-#
 #$1 username: rendgeor
 #$2 cvs repository server: cvs.berlios.de
 #$3 cvs repository path: /cvsroot/kobold
 #$4 file/s: "bla.c bla.h" (blank->commit all)
-#$5 cvs message: "added bloop"
+#$5 cvs message: "bloop"
 
 #LOGIN wird vorrausgestzt-->erzeugt .cvspass
 #cvs -d :pserver:anonymous@cvs.berlios.de:/cvsroot/kobold login 
-cvs -z3 -d :pserver:$1@$2:$3 commit -m$4
+cvs -z3 -d :pserver:$2@$3:$4 commit $5 #-m $5
 
