b0d7cb5d35516487f4b5c7b26f5d18550733d8c7
garbeam <garbeam>
2004-05-16 20:36:41 +0000

    new serialization interface for data types which are handled over XMLRPC interface

---
+++ kobold/src/kobold.common/src/kobold/common/data/ISerializable.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2003 - 2004 Necati Aydin, Armin Cont, Bettina Druckenmueller,
+ * Anselm Garbe, Michael Grosse, Tammo van Lessen, Martin Plies, Oliver Rendgen,
+ * Patrick Schneider
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * $Id: ISerializable.java,v 1.1 2004/05/16 20:36:41 garbeam Exp $
+ *  
+ */
+package kobold.common.data;
+
+import org.dom4j.Element;
+
+/**
+ * @author garbeam
+ */
+public interface ISerializable {
+	
+	/**
+	 * Serialize method.
+	 * @return Element of the serialized object. 
+	 */
+	Element serialize();
+	
+	/**
+	 * Deserialize method.
+	 * @param element an element which represents the object.
+	 */
+	void deserialize(Element element);	
+
+}
