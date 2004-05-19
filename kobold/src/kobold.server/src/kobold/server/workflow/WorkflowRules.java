380c4db9d64b94dd03862ffd222796c2707fe81d
grosseml <grosseml>
2004-05-19 13:52:39 +0000

    Regel1 TUUUT

--- kobold/src/kobold.server/src/kobold/server/workflow/WorkflowRules.java
+++ kobold/src/kobold.server/src/kobold/server/workflow/WorkflowRules.java
@@ -9,7 +9,6 @@ package kobold.server.workflow;
 
 import org.drools.rule.*;
 import kobold.common.data.*;
-import kobold.server.controller.GlobalMessageContainer;
 
 import org.drools.semantics.java.*;
 
@@ -26,18 +25,17 @@ public class WorkflowRules {
 	
 	protected static RuleSet createRuleSet() throws Exception {
 		RuleSet set = new RuleSet("Kobold");
-		WorkflowMessage msg = new WorkflowMessage("bla");
+		kobold.common.data.WorkflowMessage msg = new kobold.common.data.WorkflowMessage("Core Group Suggestion");
 		/**
 		 * Rule No.1
 		 */
 		Rule no1 = new Rule("Core Group Suggestion - Step 1");
-		
 		Declaration dec1 = new Declaration(new ClassObjectType(WorkflowMessage.class), "msg");
 		
-		no1.addCondition(new ExprCondition("(msg.getWorkflowId().equals(\"Core Group Suggestion\")) && (msg.getStep == 1)", new Declaration[] {dec1}));
+		no1.addCondition(new ExprCondition("(msg.getWorkflowType().equals(\"Core Group Suggestion\")) && (msg.getStep() == 1)", new Declaration[] {dec1}));
 		no1.addParameterDeclaration(dec1);
 		no1.setConsequence(new BlockConsequence(""+
-			"WorkflowMessage answer = new WorkflowMessage(msg.getWorkflowId());"+
+			"kobold.common.data.WorkflowMessage answer = new kobold.common.data.WorkflowMessage(msg.getWorkflowType());"+
 			"answer.addParentId(msg.getId());"+	//add the actual parent as history
 			"answer.setSender(msg.getSender());"+	
 			"answer.setReceiver(msg.getReceiver());"+
@@ -49,15 +47,16 @@ public class WorkflowRules {
 			//creating necessary workflow items
 			//containing 2 radio buttons (forward, deny)
 			//and a textbox for comments
-			"WorkflowItem text = new WorkflowItem(\"\",\"Ihr Kommentar:\",\"TEXT\");"+
-			"WorkflowItem rb1 = new WorkflowItem(\"true\",\"An zuständigen PE weiterleiten\", \"RADIO\");"+
-			"WorkflowItem rb2 = new WorkflowItem(\"false\",\"Ablehnen\", \"RADIO\");"+
-			"WorkflowItem cont = new WorkflowItem(null,\"Entscheidung\",\"CONTAINER\");"+
+			"kobold.common.data.WorkflowItem text = new kobold.common.data.WorkflowItem(\"\",\"Ihr Kommentar:\",kobold.common.data.WorkflowItem.TEXT);"+
+			"kobold.common.data.WorkflowItem rb1 = new kobold.common.data.WorkflowItem(\"true\",\"An zuständigen PE weiterleiten\",kobold.common.data.WorkflowItem.RADIO);"+
+			"kobold.common.data.WorkflowItem rb2 = new kobold.common.data.WorkflowItem(\"false\",\"Ablehnen\", kobold.common.data.WorkflowItem.RADIO);"+
+			"kobold.common.data.WorkflowItem cont = new kobold.common.data.WorkflowItem(\"bla\",\"Entscheidung\",kobold.common.data.WorkflowItem.CONTAINER);"+
 			"cont.addChild(rb1);"+
 			"cont.addChild(rb2);"+
 			
 			"answer.addWorkflowControl(cont);"+
 			"answer.addWorkflowControl(text);"));
+		
 /*			"msg.addHistory(msg.newHistory());" +			"if (msg.getHistoryList().size() > 1) {" +			"msg.getHistoryList().get(msg.getHistoryList().size() - 1).setStep(msg.getHistoryList().get(msg.getHistoryList().size() - 2).getStep() + 1);" +			"}" +			"else {" +			"msg.getHistoryList().get(msg.getHistoryList().size() - 1).setStep(1);" +			"}" +			"msg.getHistoryList().get(msg.getHistoryList().size() - 1).setFiredRule(\"Core Group Suggestion - Step 1\");" +			"msg.getHistoryList().get(msg.getHistoryList().size() - 1).setSender(msg.getSender());" +			"msg.setRecipient(\"Recipient\");" +			"msg.setMessageText(\"<?xml version = \\\"1.0\\\"?>" + 
 				"<?xml-stylesheet type = \\\"text/xsl\\\" href = \\\"styleSheets/workflow.xsl\\\" ?>" +
 				"<workflow>" + 
@@ -79,23 +78,23 @@ public class WorkflowRules {
 		
 		Declaration dec2 = new Declaration(new ClassObjectType(WorkflowMessage.class), "msg");
 		
-		no2.addCondition(new ExprCondition("(msg.getWorkflowId().equals(\"Core Group Suggestion\")) && (msg.getStep == 2)", new Declaration[] {dec2}));
+		no2.addCondition(new ExprCondition("(msg.getWorkflowType().equals(\"Core Group Suggestion\")) && (msg.getStep() == 2)", new Declaration[] {dec2}));
 		no2.addParameterDeclaration(dec1);
 		no2.setConsequence(new BlockConsequence(
-		"WorkflowMessage answer = new WorkflowMessage(msg.getWorkflowType());"+
+		"kobold.common.data.WorkflowMessage answer = new kobold.common.data.WorkflowMessage(msg.getWorkflowType());"+
 		"answer.setSender(msg.getSender());"+
 		"answer.addParentId(msg.getId());"+
 		
 		//analyzing the answer
 		"String decision = \"\";"+
 		"String comment = \"\";"+
-		"WorkflowItem controls[] = msg.getWorkflowControls();"+
+		"kobold.common.data.WorkflowItem controls[] = msg.getWorkflowControls();"+
 		"for(int i = 0; i < controls.length;i++){"+
-			"if (controls[i].getType().equals(\"TEXT\")){"+
+			"if (controls[i].equals(kobold.common.data.kobold.common.data.WorkflowItem.TEXT)){"+
 				"comment = controls[i].getValue();"+
 			"}"+
-			"if (controls[i].getType().equals(\"CONTAINER\")){"+
-				"WorkflowItem container[] = controls[i].getChildren();"+
+			"if (controls[i].equals(kobold.common.data.kobold.common.data.WorkflowItem.CONTAINER)){"+
+				"kobold.common.data.WorkflowItem container[] = controls[i].getChildren();"+
 				"for (int j = 0; j < container.length;j++){"+
 					"if (container[j].getValue().equals(\"true\")){"+
 						"decision = container[j].getDescription();"+
@@ -113,10 +112,10 @@ public class WorkflowRules {
 					"\"der zuständige PE schrieb dazu: \" + comment);"+
 			
 			//add decisions for core group PE
-			"WorkflowItem radio1 = new WorkflowItem(\"false\",\"Vorschlag zustimmen\",\"RADIO\");"+
-			"WorkflowItem radio2 = new WorkflowItem(\"true\",\"Vorschlag ablehnen\",\"RADIO\");"+
-			"WorkflowItem comm = new WorkflowItem(\"\",\"Bitte hier ein Kommentar einfügen:\",\"TEXT\");"+
-			"WorkflowItem container = new WorkflowItem(null,\"Entscheidung\",\"CONTAINER\");"+
+			"kobold.common.data.WorkflowItem radio1 = new kobold.common.data.WorkflowItem(\"false\",\"Vorschlag zustimmen\",kobold.common.data.kobold.common.data.WorkflowItem.RADIO);"+
+			"kobold.common.data.WorkflowItem radio2 = new kobold.common.data.WorkflowItem(\"true\",\"Vorschlag ablehnen\",kobold.common.data.kobold.common.data.WorkflowItem.RADIO);"+
+			"kobold.common.data.WorkflowItem comm = new kobold.common.data.WorkflowItem(\"\",\"Bitte hier ein Kommentar einfügen:\",kobold.common.data.kobold.common.data.WorkflowItem.TEXT);"+
+			"kobold.common.data.WorkflowItem container = new kobold.common.data.WorkflowItem(null,\"Entscheidung\",kobold.common.data.kobold.common.data.WorkflowItem.CONTAINER);"+
 			"container.addChild(radio1);"+
 			"container.addChild(radio2);"+
 			"answer.addWorkflowControl(comm);"+
@@ -124,8 +123,8 @@ public class WorkflowRules {
 		"}"+
 		
 		"else if (decision.equals(\"Ablehnen\")){"+
-			"GlobalMessageContainer gmc = GlobalMessageContainer.getInstance();"+
-			"WorkflowMessage wfm = (WorkflowMessage)gmc.getMessage(msg.getParentIds()[0]);"+
+			"kobold.server.controller.GlobalMessageContainer gmc = kobold.server.controller.GlobalMessageContainer.getInstance();"+
+			"kobold.common.data.WorkflowMessage wfm = (kobold.common.data.WorkflowMessage)gmc.getMessage(msg.getParentIds()[0]);"+
 			"answer.setReceiver(wfm.getSender());"+
 			"answer.setSubject(\"Ihre Anfrage wurde abgelehnt.\");"+
 			"answer.setMessageText(\"Ihre Anfrage wurde von \"+msg.getSender() +\" mit der Begründung '\" +"+
@@ -176,23 +175,23 @@ public class WorkflowRules {
 		
 		Declaration dec3 = new Declaration(new ClassObjectType(WorkflowMessage.class), "msg");
 		
-		no3.addCondition(new ExprCondition("(msg.getWorkflowId().equals(\"Core Group Suggestion\")) && (msg.getStep == 2)", new Declaration[] {dec3}));
+		no3.addCondition(new ExprCondition("(msg.getWorkflowType().equals(\"Core Group Suggestion\")) && (msg.getStep() == 2)", new Declaration[] {dec3}));
 		no3.addParameterDeclaration(dec1);
 		no3.setConsequence(new BlockConsequence(
-				"WorkflowMessage answer = new WorkflowMessage(msg.getWorkflowType());"+
+				"kobold.common.data.WorkflowMessage answer = new kobold.common.data.WorkflowMessage(msg.getWorkflowType());"+
 				"answer.setSender(msg.getSender());"+
 				"answer.addParentId(msg.getId());"+
 				
 				//analyzing the answer
 				"String decision = \"\";"+
 				"String comment = \"\";"+
-				"WorkflowItem controls[] = msg.getWorkflowControls();"+
+				"kobold.common.data.WorkflowItem controls[] = msg.getWorkflowControls();"+
 				"for(int i = 0; i < controls.length;i++){"+
 					"if (controls[i].getType().equals(\"TEXT\")){"+
 						"comment = controls[i].getValue();"+
 					"}"+
 					"if (controls[i].getType().equals(\"CONTAINER\")){"+
-						"WorkflowItem container[] = controls[i].getChildren();"+
+						"kobold.common.data.WorkflowItem container[] = controls[i].getChildren();"+
 						"for (int j = 0; j < container.length;j++){"+
 							"if (container[j].getValue().equals(\"true\")){"+
 								"decision = container[j].getDescription();"+
@@ -201,16 +200,16 @@ public class WorkflowRules {
 					"}"+
 				"}"+
 			"if (decision.equals(\"Vorschlag zustimmen\")){"+
-				"GlobalMessageContainer gmc = GlobalMessageContainer.getInstance();"+
-				"WorkflowMessage wfm = (WorkflowMessage)gmc.getMessage(msg.getParentIds()[0]);"+
+				"kobold.server.controller.GlobalMessageContainer gmc = GlobalMessageContainer.getInstance();"+
+				"kobold.common.data.WorkflowMessage wfm = (kobold.common.data.WorkflowMessage)gmc.getMessage(msg.getParentIds()[0]);"+
 				"answer.setReceiver(wfm.getSender());"+
 				"answer.setSubject(\"Ihr Vorschlag wurde akzeptiert.\");"+
 				"answer.setMessageText(\"Ihre Anfrage wurde von \"+msg.getSender() +\" akzeptiert. Weitere Anweisungen lauten: '\" +"+
 						 "comment +\"'\");"+
 			"}"+
 			"else if (decision.equals(\"Vorschlag ablehnen\")){"+
-				"GlobalMessageContainer gmc = GlobalMessageContainer.getInstance();"+
-				"WorkflowMessage wfm = (WorkflowMessage)gmc.getMessage(msg.getParentIds()[0]);"+
+				"kobold.server.controller.GlobalMessageContainer gmc = GlobalMessageContainer.getInstance();"+
+				"kobold.common.data.WorkflowMessage wfm = (kobold.common.data.WorkflowMessage)gmc.getMessage(msg.getParentIds()[0]);"+
 				"answer.setReceiver(wfm.getSender());"+
 				"answer.setSubject(\"Ihre Anfrage wurde abgelehnt.\");"+
 				"answer.setMessageText(\"Ihre Anfrage wurde von \"+msg.getSender() +\" mit der Begründung '\" +"+
