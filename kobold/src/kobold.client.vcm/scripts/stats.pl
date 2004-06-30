d912c74900a860e294a74c2621574f833bc9f84c
rendgeor <rendgeor>
2004-06-30 22:43:46 +0000

    fixed script

--- kobold/src/kobold.client.vcm/scripts/stats.pl
+++ kobold/src/kobold.client.vcm/scripts/stats.pl
@@ -1,40 +1,76 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 #
 # See ../LICENSE.txt for license information.
 #
 
 # specified output format
-# <type>\t<path>\t<revision>\t<date>\t<flags>
+# <type>\t<path>\t<revision>\t<date>\t<tag>
 
-# TODO: check OS
+# TODO: check OS --> current directory solved!
+#       confusing warnings perl -w ?
 
 use strict;
+use Cwd;
 
 sub read_entries {
     my $root = shift; # shifted at next argument in @_
     my $filename = "$root/CVS/Entries";
+    my @temp;
 
-    open(FILE, "< $filename" )
-        or die "Can't open $filename : $!";
+    #empty directories with no included data aren't
+    #checked out! --> don't display a message
+    open(FILE, "< $filename" );
+    #    or die "Can't open $filename : $!";
 
-    my ($type, $path, $rev ,$date, $tag);
-    $type = $path = $rev = $date = $tag = "";
+    #for all lines (filenames)
     while(<FILE>) {
-        chomp;
-        if (!($_ eq "D")) {
-            my ($type, $path, $rev ,$date, $tag) = split( '\/' );
-            $_ = $tag;
+        #chomp;
+        my ($type, $path, $rev ,$date, $tag) = split( '\/' );
+        
+        #print "KOMPLETT: $type, $path, $rev ,$date, $tag\n";
+
+
+        #if the actual line ($_) represent a file
+        if ($type ne "D") {
+            #pretty printing "binary"
             $tag =~ s/-kb/binary/g;
-            if ($type eq "D") {
-                read_entries("$root/$path");
-            }
+
             print "$root/$path\t$rev\t$date\t$tag\n";
         }
+        
+        #for all directories, call recursive
+
+        #store all directories because when opening another file the old
+        #isn't accessable!!!
+        elsif ($type eq "D" ) 
+        {
+            #handling for error-lines only consist of "D"
+            if (length($path) > 0)
+            {
+                push (@temp, "$root/$path");
+                #doesn't work: read_entries("$root/$path");
+            }
+       }
+            
     }
+    close (FILE);
+
+    #now do all subdirectories
+    #print "ALLE: @temp\n";
+    foreach my $array_element (@temp)
+    {
+        #print the directory name
+        print "$array_element\n";
+        read_entries ($array_element);
+    }
+
+
+
 }
 
-my $currDir = `pwd`;
-chomp $currDir;
+###MAIN###
+
+my $currDir = cwd;#`pwd`;#chomp $currDir;
 
 #print "beginning with: $currDir\n";
 read_entries($currDir);
