2648bc4322e8077dc60dbf4d5c8fb83c30066597
martinplies <martinplies>
2004-07-15 02:53:42 +0000

    change name

--- kobold/src/kobold.common/src/kobold/common/exception/GXLException.java
+++ kobold/src/kobold.common/src/kobold/common/exception/GXLException.java
@@ -7,8 +7,7 @@
 package kobold.common.exception;
 
 /**
- * @author meiner
- *
+ * @author pliesmn *
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
