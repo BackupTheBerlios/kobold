cee75174b84aab7122150a7e09cd3c915de557fd
martinplies <martinplies>
2004-07-15 02:51:01 +0000

    add license

--- kobold/src/kobold.client.plam/src/kobold/client/plam/model/edges/EdgeContainer.java
+++ kobold/src/kobold.client.plam/src/kobold/client/plam/model/edges/EdgeContainer.java
@@ -1,4 +1,28 @@
 /*
+ * Copyright (c) 2003 - 2004 Necati Aydin, Armin Cont, 
+ * Bettina Druckenmueller, Anselm Garbe, Michael Grosse, 
+ * Tammo van Lessen,  Martin Plies, Oliver Rendgen, Patrick Schneider
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *
+ *//*
  * Created on 07.07.2004
  *
  * TODO To change the template for this generated file go to
@@ -19,10 +43,9 @@ import kobold.common.data.ISerializable;
 import org.dom4j.Element;
 
 /**
- * @author Martin Plies
+ * @author pliesmn
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
  */
 public class EdgeContainer implements ISerializable {
     
@@ -55,7 +78,7 @@ public class EdgeContainer implements ISerializable {
       // create Edge
       List edges = ((List) nodes.get(startNode));
       if (! containsEdge(edges, targetNode)){
-          edges.add(new BauhausEdge(startNode, targetNode, type, number));
+          edges.add(new Edge(startNode, targetNode, type, number));
       }
   }
   
--- kobold/src/kobold.client.plam/src/kobold/client/plam/model/edges/MetaNode.java
+++ kobold/src/kobold.client.plam/src/kobold/client/plam/model/edges/MetaNode.java
@@ -1,9 +1,29 @@
 /*
- * Created on 07.07.2004
+ * Copyright (c) 2003 - 2004 Necati Aydin, Armin Cont, 
+ * Bettina Druckenmueller, Anselm Garbe, Michael Grosse, 
+ * Tammo van Lessen,  Martin Plies, Oliver Rendgen, Patrick Schneider
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * $Id: MetaNode.java,v 1.3 2004/07/15 02:55:53 martinplies Exp $
+ *
+ */ 
 package kobold.client.plam.model.edges;
 
 /**
@@ -12,7 +32,7 @@ package kobold.client.plam.model.edges;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class MetaNode implements INode {
+public class MetaNode  implements INode {
    public static final String OR = "OR";
    public static final String XOR = "XOR";
    public static final String AND = "AND";
