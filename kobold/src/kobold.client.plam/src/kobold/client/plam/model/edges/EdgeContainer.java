14cd4f73aa44653c872ea001cd38dffcfd840853
martinplies <martinplies>
2004-07-22 16:16:03 +0000

    refactor this class and add getEdgesFrom() and getEdgesTo ()

--- kobold/src/kobold.client.plam/src/kobold/client/plam/model/edges/EdgeContainer.java
+++ kobold/src/kobold.client.plam/src/kobold/client/plam/model/edges/EdgeContainer.java
@@ -38,11 +38,17 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 
+import kobold.client.plam.model.AbstractRootAsset;
 import kobold.common.data.ISerializable;
 
 import org.dom4j.Element;
 
 /**
+ * An EdgeContainer contains all edges of a product or a 
+ * productline. The Edges are magaged form their EdgeContainer.
+ * All Edges are directed. To get a fast Return. 
+ * The EdgeContainer has two Adazentslists to manage the edges.  
+ * 
  * @author pliesmn
  *
  * 
@@ -50,36 +56,83 @@ import org.dom4j.Element;
 public class EdgeContainer implements ISerializable {
     
   
-  Map nodes = new HashMap();
+  Map startNodesList = new HashMap();
+  Map targetNodesList = new HashMap();
+  AbstractRootAsset root; 
   
   
-  
-  public EdgeContainer() {
-      
+  public EdgeContainer(AbstractRootAsset root) {
+      this.root = root;
   }
   
-  public void addNode(INode node){
-      if (! nodes.containsKey(node)){
-          nodes.put(node, new ArrayList());
+  
+  
+  /**
+   * Creates and adds a node to this edgecontainer. 
+   * The nummer tells, for how many edges this edge represent.
+   * Number is only an additonal Information for the user. 
+   * Number is only user of extern edges.
+   * If the target node or the start node are not understartNodes of
+   * the root asset of this conatainer or if the edge exitst, this method return false
+   * and the edge is not created.
+   * If the edge is added this method return true; 
+   * 
+ * @param startNode
+ * @param targetNode
+ * @param type
+ * @param number
+ * @return true, if the edege can added.
+ */
+public boolean addEdge(INode startNode, INode targetNode, String type, int number){
+      if (startNode.getRoot()!= root || targetNode.getRoot() != root){          
+          return false;
       }
-  }
-  
-  public void addEdge(INode startNode, INode targetNode){
-      addNode(startNode);
+      addStartNode(startNode);
+      addTragetNode(targetNode);      
       // create Edge
-      List edges = ((List) nodes.get(startNode));
-      if (! containsEdge(edges, targetNode)){
-          edges.add(new Edge(startNode, targetNode));
-      } 
-  }
-  
-  public void addBauhausEdge(INode startNode, INode targetNode, String type, int number){
-      addNode(startNode);
-      // create Edge
-      List edges = ((List) nodes.get(startNode));
+      List edges = ((List) startNodesList.get(startNode));
       if (! containsEdge(edges, targetNode, type)){
-          edges.add(new Edge(startNode, targetNode, type, number));
+          Edge newEdge = new Edge(startNode, targetNode, type, number);
+          edges.add(newEdge);          
+          // addEdge also to targetNodesList
+          ((List) targetNodesList.get(targetNode)).add(newEdge);
+          return true;
       }
+      return false;
+  }
+  
+
+  /**
+ * @param targetNode
+ */
+private void addTragetNode(INode targetNode) {
+  if (! targetNodesList.containsKey(targetNode)){
+      targetNodesList.put(targetNode, new ArrayList());
+  }
+}
+
+/**
+ * @param startNode
+ */
+private void addStartNode(INode startNode) {
+    if (! startNodesList.containsKey(startNode)){
+        startNodesList.put(startNode, new ArrayList());
+    }
+}
+
+/**
+   * Creates and adds a node to this edgecontainer. 
+   * If the target node or the start node are not understartNodes of
+   * the root asset of this conatainer or if the edge exitst, this method return false
+   * and the edge is not created.
+   * If the edge is added this method return true; 
+   * 
+ * @param startNode
+ * @param targetNode
+ * @param type
+ */
+public boolean addEdge(INode startNode, INode targetNode, String type){
+     return addEdge(targetNode,targetNode, type, 1);
   }
   
   private boolean containsEdge(List edges, INode targetNode) {
@@ -93,9 +146,15 @@ public class EdgeContainer implements ISerializable {
       return false;      
   }
   
-  public boolean containsEdge(INode startNode, INode targetNode) {
-      if (this.nodes.containsKey(startNode)){
-        return containsEdge((List) this.nodes.get(startNode), targetNode);
+  
+  /**
+ * @param startNode
+ * @param targetNode
+ * @return returns true, if an edge exists from the start node to the target node, otherwise false.
+ */
+public boolean containsEdge(INode startNode, INode targetNode) {
+      if (this.startNodesList.containsKey(startNode)){
+        return containsEdge((List) this.startNodesList.get(startNode), targetNode);
       }  else { return false; }      
   }
   
@@ -110,66 +169,80 @@ public class EdgeContainer implements ISerializable {
       return false;      
   }
   
-  private boolean containsEdge (INode startNode, INode targetNode, String type){
-      if (this.nodes.containsKey(startNode)){
-          return containsEdge((List) this.nodes.get(startNode), targetNode, type);
+  /**
+ * @param startNode
+ * @param targetNode
+ * @param type
+ * @return returns true, if an edge with this type exists form the start node to the target node , otherwise flase.
+ */
+private boolean containsEdge (INode startNode, INode targetNode, String type){
+      if (this.startNodesList.containsKey(startNode)){
+          return containsEdge((List) this.startNodesList.get(startNode), targetNode, type);
         }  else { return false; }  
   }
  
+
+
   
   public void removeEdges(INode startNode, INode targetNode){
-      if (this.nodes.containsKey(startNode)){
-          List edges = (List) nodes.get(startNode);
+      if (this.startNodesList.containsKey(startNode)){
+          List edges = (List) startNodesList.get(startNode);
           for (ListIterator ite = edges.listIterator(); ite.hasNext();){
               Edge edge = (Edge) ite.next();
               if(edge.getTargetNode().equals(targetNode)){
                  edges.remove(edge);
               }
           }
-      }  
-  }
-  
-  
-  public void removeEdge(INode startNode, INode targetNode, String type){
-      if (this.nodes.containsKey(startNode)){
-          List edges = (List) nodes.get(startNode);
+          // remove also from targetNodesList
+          edges = (List) targetNodesList.get(targetNode);
           for (ListIterator ite = edges.listIterator(); ite.hasNext();){
               Edge edge = (Edge) ite.next();
-              if(edge.getTargetNode().equals(targetNode) && edge.getType().equals(type)){
+              if(edge.getStartNode().equals(startNode)){
                  edges.remove(edge);
-                 return;
               }
           }
+          
       }  
   }
   
   
   /**
-   * 
-   * @return
-   */
-  public Edge getEdge(INode startNode, INode targetNode){
-    if (this.nodes.containsKey(startNode)) {
-      return getEdge((List) nodes.get(startNode), targetNode);  
-    }  else {
-        return null;
-    }
+   * Remove the edge from the start node to the target node with this type.
+ * @param startNode
+ * @param targetNode
+ * @param type
+ */
+public void removeEdge(INode startNode, INode targetNode, String type){
+      if (this.startNodesList.containsKey(startNode)){
+          List edges = (List) startNodesList.get(startNode);
+          for (ListIterator ite = edges.listIterator(); ite.hasNext();){
+              Edge edge = (Edge) ite.next();
+              if(edge.getTargetNode().equals(targetNode) && edge.getType().equals(type)){
+                 edges.remove(edge); 
+              }
+          }
+          // removes also from targetNodesList
+          edges = (List) targetNodesList.get(targetNode);
+          for (ListIterator ite = edges.listIterator(); ite.hasNext();){
+              Edge edge = (Edge) ite.next();
+              if(edge.getStartNode().equals(startNode) && edge.getType().equals(type)){
+                 edges.remove(edge);
+              }
+          }
+      }  
   }
   
-  private Edge getEdge(List edges, INode targetNode){
-      for (Iterator ite = edges.iterator(); ite.hasNext();){
-          Edge edge = (Edge) ite.next();
-          if (edge.getTargetNode().equals(targetNode)) {
-              return edge;
-          }
-      }
-      return null;
-   }
-  
-  private Edge[] getEdges(INode startNode, INode targetNode) {
-    if (! nodes.containsKey(startNode)) {return new Edge[0];}
+ 
+  /**
+   * 
+ * @param startNode
+ * @param targetNode
+ * @return all edges, that start at the startNode and end at the targetNode
+ */
+private Edge[] getEdges(INode startNode, INode targetNode) {
+    if (! startNodesList.containsKey(startNode)) {return new Edge[0];}
     List list = new LinkedList();
-    List edges = (List) nodes.get(startNode);
+    List edges = (List) startNodesList.get(startNode);
     for (Iterator ite = edges.iterator(); ite.hasNext();){
         Edge edge = (Edge) ite.next();
         if (edge.getTargetNode().equals(targetNode)){
@@ -178,11 +251,49 @@ public class EdgeContainer implements ISerializable {
     }
     return (Edge[]) list.toArray(new Edge[0]);
   }
+
+ /**
+  * Returns an Edgearray with all Edges from startNode. 
+  * If no edges exists from this node the, an empty Edgearray is returned.
+ * @param startNode
+ * @return Returns an Edgearray with all Edges from startNode. 
+ */
+public Edge[] getEdgesFrom(INode startNode){
+     if (startNodesList.containsKey(startNode)){
+         return (Edge[]) ((List) startNodesList.get(startNode)).toArray(new Edge[0]);
+     } else {
+         return new Edge[0];
+     }   
+ }
+ 
+ /**
+  * Returns an Edgearray with all Edges from targetNode.
+  * If no edges exists to this node the, an empty Edgearray is returned.
+ * @param targetNode
+ * @return Returns an Edgearray with all Edges from targetNode.
+ */
+public Edge[] getEdgesTo(INode targetNode){
+     if(targetNodesList.containsKey(targetNode)){
+         return (Edge[]) ((List) targetNodesList.get(targetNode)).toArray(new Edge[0]);
+     } else {
+         return new Edge[0];
+     }
+ }
   
-  private Edge getEdge(List edges, INode targetNode, String Type){
+
+
+  /**
+   * Retruns the edge, or null, if the edge not exists.
+ * @param edges
+ * @param targetNode
+ * @param type
+ * @return Returns edge. 
+ */
+private Edge getEdge(List edges, INode targetNode, String type){
       for (Iterator ite = edges.iterator(); ite.hasNext();){
           Edge edge = (Edge) ite.next();
-          if (edge.getTargetNode().equals(targetNode)) {
+          if (edge.getTargetNode().equals(targetNode)
+              && edge.getType().equals(type) ) {
               return edge;
           }
       }
@@ -190,10 +301,9 @@ public class EdgeContainer implements ISerializable {
    }
   
   
-  
  
   public Element serialize() {
-     //TODO
+     // TODO
       return null;
   }
 
