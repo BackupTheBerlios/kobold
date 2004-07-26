8d6f32ab9f05968e75ea1cec3ec88feb6d1158c3
bettina <bettina>
2004-07-26 17:29:54 +0000

    *** empty log message ***

--- kobold/docs/handbuch/appendix.pl
+++ kobold/docs/handbuch/appendix.pl
@@ -8,9 +8,83 @@
 use strict;
 
 print "{\\small \\begin{verbatim}";
+
 open(FILE, "../../src/kobold.common/src/kobold/common/controller/IKoboldServer.java" );
 while(<FILE>) {
     print $_;
 }
 close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/AbstractKoboldMessage.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/Asset.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/Component.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/IdManager.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/KoboldMessage.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/Product.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/Productline.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/RPCSpy.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/User.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/UserContext.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/WorkflowItem.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
+open(FILE, "../../src/kobold.common/src/kobold/common/data/WorkflowMessage.java" );
+while(<FILE>) {
+    print $_;
+}
+close (FILE);
+
 print "\\end{verbatim}}";
