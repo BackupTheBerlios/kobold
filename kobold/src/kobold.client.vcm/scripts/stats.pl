9a18ec139b6e3936a375861b1e6171fe06006fa8
rendgeor <rendgeor>
2004-07-28 22:22:04 +0000

    fixed script to recognize also phisical files

--- kobold/src/kobold.client.vcm/scripts/stats.pl
+++ kobold/src/kobold.client.vcm/scripts/stats.pl
@@ -9,13 +9,29 @@
 # TODO: check OS --> current directory solved!
 #       confusing warnings perl -w ?
 
+#Datentypen:
+# $ für Skalar,
+# @ für Arrays,
+# % für Hash,
+# & für Funktion.
+
 use strict;
 use Cwd;
 
+#for listing all files
+use File::Find;
+
+#the global hash map
+my %data;
+
+
 sub read_entries {
+
     my $root = shift; # shifted at next argument in @_
     my $filename = "$root/CVS/Entries";
     my @temp;
+
+
 
     #empty directories with no included data aren't
     #checked out! --> don't display a message
@@ -25,17 +41,25 @@ sub read_entries {
     #for all lines (filenames)
     while(<FILE>) {
         #chomp;
+        
+        #split each line by "/"
         my ($type, $path, $rev ,$date, $tag) = split( '\/' );
         
         #print "KOMPLETT: $type, $path, $rev ,$date, $tag\n";
+     
 
 
         #if the actual line ($_) represent a file
         if ($type ne "D") {
-            #pretty printing "binary"
-            $tag =~ s/-kb/binary/g;
+            #empty lines
+            if (length($path) > 0) {
+                #pretty printing "binary"
+                $tag =~ s/-kb/binary/g;
 
-            print "$root/$path\t$rev\t$date\t$tag\n";
+                #!print "$root/$path\t$rev\t$date\t$tag\n";
+                #store all in the hash map instead
+                $data {"$root/$path"} = "$rev\t$date\t$tag";
+            }
         }
         
         #for all directories, call recursive
@@ -48,7 +72,7 @@ sub read_entries {
             if (length($path) > 0)
             {
                 push (@temp, "$root/$path");
-                #doesn't work: read_entries("$root/$path");
+                #doesn't work recursive: read_entries("$root/$path");
             }
        }
             
@@ -60,7 +84,11 @@ sub read_entries {
     foreach my $array_element (@temp)
     {
         #print the directory name
-        print "$array_element\n";
+        #directories: no need to store here!
+        #!print "$array_element\n";
+
+
+        #process all directories stored in @temp
         read_entries ($array_element);
     }
 
@@ -71,13 +99,36 @@ sub read_entries {
 if ($#ARGV == 0)
 {
     #or the current directory?
-    #my $currDir = cwd;#`pwd`; #chomp $currDir;
+    #my $currDir = cwd;#`pwd`; 
+    #chomp $currDir;
+
+    #the path to start to parse
     my $currDir = $ARGV[0];
+
+    my $root = shift; # shifted at next argument in @_
+
+########ALL physical files and directories
+    sub show{
+        #print ("$File::Find::name \n");
+        
+        #store all in the hash map instead
+        $data {"$File::Find::name"} = "";
+    }
+    find (\&show,$currDir);
+
+
+##########
 
     #print "beginning with: $currDir\n";
     read_entries($currDir);
+
+    #now print all entries of the hash map
+    foreach my $key ( keys %data){
+        print $key."\t".$data{$key}."\n"
+    }
 }
+
 else
 {
-    print "give me argument path!\n";
+    print "give me argument: path to parse!\n";
 }
