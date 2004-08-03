ddef864fb983896e61c28ee230894b389906949b
neco <neco>
2004-08-03 23:53:18 +0000

    *** empty log message ***

--- kobold/src/kobold.client.plam/src/kobold/client/plam/MetaInformation.java
+++ kobold/src/kobold.client.plam/src/kobold/client/plam/MetaInformation.java
@@ -52,13 +52,12 @@ public class MetaInformation {
             // add a paragraph to the document
             document.add(new Paragraph("Meta-Informations"));
             document.add(new Paragraph("Productline:"));	
-            Productline pl = new Productline();
-            pl.setName("PL");
+            Productline pl = new Productline("PL");
             document.add(new Phrase(pl.getName()));
             document.add(new Phrase(pl.getDescription()));
             document.add(new Phrase("Maintainers"));
             document.add(new Paragraph("Products:"));	
-         	
+                    	
         }
         catch(DocumentException de) {
             System.err.println(de.getMessage());
@@ -71,41 +70,42 @@ public class MetaInformation {
         document.close();
     }
  	//MetaInformation for Productlines
-    public void createMetaInfoForPL (Document document, Productline pLine) {
-	
-    	
-        Productline pl = new Productline();
-        pl.setName("PL");
-       
-        for (Iterator ite = pl.getMaintainers().iterator();
-        ite.hasNext();)
-        {
-        	User u1 = (User) ite.next();
-        	createMetaInfoForMaintainer (document, u1);
-        }
-       		
+    public void createMetaInfoForPL (Document document, Productline pLine) 
+    {
+	    Productline pl = new Productline("PL");
+        //document.add(new Paragraph("Productline:"));	
+        //document.add(new Phrase(pl.getName()));
+        //document.add(new Phrase(pl.getDescription()));
+        //document.add(new Phrase("Maintainers"));
+        //document.add(new Paragraph("Products:"));	
+                 		
         for (Iterator ite = pl.getProducts().iterator(); 
 		ite.hasNext();)
         {
-			Product p1 = (Product) ite.next();
-			createMetaInfoForProduct (document, p1);
+			Product product = (Product) ite.next();
+			createMetaInfoForProduct (document, product);
 		}
     
 		for (Iterator ite = pl.getComponents().iterator(); 
 		ite.hasNext();)
 		{
-			Component c1 = (Component) ite.next();
-			createMetaInfoForComponent (document, c1);
+			Component comp = (Component) ite.next();
+			createMetaInfoForComponent (document, comp);
 		}	
-	
+		 
+        for (Iterator ite = pl.getMaintainers().iterator();
+        ite.hasNext();)
+        {
+        	User user = (User) ite.next();
+        	createMetaInfoForMaintainer (document, user);
+        }
 	
     }
     
     //MetaInformation for Products
-    public void createMetaInfoForProduct(Document document, Product p1) {
-    	
-        Product product = new Product();
-        product.setName("test");
+    public void createMetaInfoForProduct(Document document, Product p1) 
+    {
+    	Product product = new Product("Product1");
         product.getName();
     	product.getDescription();
     	product.getMaintainers();
@@ -113,33 +113,42 @@ public class MetaInformation {
     	for (Iterator ite = product.getRelatedComponents().iterator(); 
 		ite.hasNext();)
 		{
-			RelatedComponent rc1 = (RelatedComponent) ite.next();
-			createMetaInfoForRelatedComponent(document, rc1);
+			RelatedComponent relComp = (RelatedComponent) ite.next();
+			createMetaInfoForRelatedComponent(document, relComp);
 		}
     	for (Iterator ite = product.getSpecificComponents().iterator(); 
 		ite.hasNext();)
 		{
-    		SpecificComponent spc1 = (SpecificComponent) ite.next();
-			createMetaInfoForSpecificComponents(document, spc1);
+    		SpecificComponent spComp = (SpecificComponent) ite.next();
+			createMetaInfoForSpecificComponents(document, spComp);
 		}	
 	
     }
     
     //MetaInformation for SpecificComponents	
-	private void createMetaInfoForSpecificComponents(Document document, SpecificComponent spc1) 
+	private void createMetaInfoForSpecificComponents(Document document, SpecificComponent spComp) 
 	{
-		
-	}	
-	private void createMetaInfoForComponent(Document document, Component c1)
-	{
-			
+		spComp.getName();
+		spComp.getDescription();
+		spComp.getMaintainers();
 	}
-	private void createMetaInfoForRelatedComponent(Document document, RelatedComponent rc1) 
+	
+	private void createMetaInfoForComponent(Document document, Component comp)
 	{
-		
+		comp.getName();
+		comp.getDescription();
+		comp.getMaintainers();	
 	}
-	private void createMetaInfoForMaintainer(Document document, User u1) 
+	
+	private void createMetaInfoForRelatedComponent(Document document, RelatedComponent relComp) 
 	{
-			
+		relComp.getName();
+		relComp.getDescription();
+		relComp.getMaintainers();
+	}
+	
+	private void createMetaInfoForMaintainer(Document document, User user) 
+	{
+		user.getUsername();	
 	}
 }
