f44b09a16c30f2dd34ca353e358e213d5691a87a
neco <neco>
2004-08-03 17:29:52 +0000

    *** empty log message ***

---
+++ kobold/src/kobold.client.plam/src/kobold/client/plam/MetaInformation.java
@@ -0,0 +1,142 @@
+/*
+ * Created on 14.07.2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package kobold.client.plam;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import kobold.client.plam.model.product.Product;
+import kobold.client.plam.model.product.RelatedComponent;
+import kobold.client.plam.model.product.SpecificComponent;
+import kobold.client.plam.model.productline.Component;
+import kobold.client.plam.model.productline.Productline;
+import kobold.common.data.User;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfWriter;
+
+
+/**
+ * @author Necati Aydin
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class MetaInformation {
+
+
+	public static void getMetaData(String[] args) {
+		System.out.println("Metadaten");
+        
+        // creation of a document-object
+        Document document = new Document();
+        
+        try {
+            
+            // create a writer that listens to the document
+            // and directs a PDF-stream to a file
+            
+            PdfWriter.getInstance(document, new FileOutputStream("MetaInfo.pdf"));
+            
+            // open the document
+            document.open();
+            
+            // add a paragraph to the document
+            document.add(new Paragraph("Meta-Informations"));
+            document.add(new Paragraph("Productline:"));	
+            Productline pl = new Productline("PL");
+            document.add(new Phrase(pl.getName()));
+            document.add(new Phrase(pl.getDescription()));
+            document.add(new Phrase("Maintainers"));
+            document.add(new Paragraph("Products:"));	
+         	
+        }
+        catch(DocumentException de) {
+            System.err.println(de.getMessage());
+        }
+        catch(IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+        
+        // close the document
+        document.close();
+    }
+ 	//MetaInformation for Productlines
+    public void createMetaInfoForPL (Document document, Productline pLine) {
+	
+    	
+        Productline pl = new Productline("PL");
+       
+        for (Iterator ite = pl.getMaintainers().iterator();
+        ite.hasNext();)
+        {
+        	User u1 = (User) ite.next();
+        	createMetaInfoForMaintainer (document, u1);
+        }
+       		
+        for (Iterator ite = pl.getProducts().iterator(); 
+		ite.hasNext();)
+        {
+			Product p1 = (Product) ite.next();
+			createMetaInfoForProduct (document, p1);
+		}
+    
+		for (Iterator ite = pl.getComponents().iterator(); 
+		ite.hasNext();)
+		{
+			Component c1 = (Component) ite.next();
+			createMetaInfoForComponent (document, c1);
+		}	
+	
+	
+    }
+    
+    //MetaInformation for Products
+    public void createMetaInfoForProduct(Document document, Product p1) {
+    	
+        Product product = new Product("Product1");
+        product.getName();
+    	product.getDescription();
+    	product.getMaintainers();
+    	
+    	for (Iterator ite = product.getRelatedComponents().iterator(); 
+		ite.hasNext();)
+		{
+			RelatedComponent rc1 = (RelatedComponent) ite.next();
+			createMetaInfoForRelatedComponent(document, rc1);
+		}
+    	for (Iterator ite = product.getSpecificComponents().iterator(); 
+		ite.hasNext();)
+		{
+    		SpecificComponent spc1 = (SpecificComponent) ite.next();
+			createMetaInfoForSpecificComponents(document, spc1);
+		}	
+	
+    }
+    
+    //MetaInformation for SpecificComponents	
+	private void createMetaInfoForSpecificComponents(Document document, SpecificComponent spc1) 
+	{
+		
+	}	
+	private void createMetaInfoForComponent(Document document, Component c1)
+	{
+			
+	}
+	private void createMetaInfoForRelatedComponent(Document document, RelatedComponent rc1) 
+	{
+		
+	}
+	private void createMetaInfoForMaintainer(Document document, User u1) 
+	{
+			
+	}
+}
