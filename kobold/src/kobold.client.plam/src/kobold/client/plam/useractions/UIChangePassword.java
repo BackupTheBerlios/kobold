c6b6cf61218429d77be2913cae240bc5e4fb982c
martinplies <martinplies>
2004-08-04 12:55:53 +0000

    update gui

--- kobold/src/kobold.client.plam/src/kobold/client/plam/useractions/UIChangePassword.java
+++ kobold/src/kobold.client.plam/src/kobold/client/plam/useractions/UIChangePassword.java
@@ -21,16 +21,24 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
  * DEALINGS IN THE SOFTWARE.
  *
- * $Id: UIChangePassword.java,v 1.4 2004/08/02 12:06:51 martinplies Exp $
+ * $Id: UIChangePassword.java,v 1.5 2004/08/04 12:55:53 martinplies Exp $
  *
  */
 package kobold.client.plam.useractions;
 
 
+import kobold.client.plam.wizard.Messages;
+
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
@@ -43,7 +51,7 @@ import org.eclipse.swt.widgets.Text;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
-public class UIChangePassword extends Dialog	{
+public class UIChangePassword extends TitleAreaDialog	{
     
     private Label labelOldPassword;
     private Label labelPassword;
@@ -52,6 +60,7 @@ public class UIChangePassword extends Dialog	{
     private Text textOldPassword;
     private Text textPassword;
     private Text textConfPass;
+    private Button okButton;
     
     /**
      * @param item
@@ -64,7 +73,7 @@ public class UIChangePassword extends Dialog	{
     
     
     public Control createDialogArea(Composite parent) {
-        
+        this.setTitle("Password Change");
         Composite control = new Composite(parent, SWT.NONE);
         GridLayout gridLayout = new GridLayout();
         gridLayout.numColumns = 2;
@@ -77,28 +86,62 @@ public class UIChangePassword extends Dialog	{
         
         //the old password
         labelOldPassword = new Label(control,SWT.NONE);
-        labelOldPassword.setText("Please insert your current password:");
-        textOldPassword = new Text(control, SWT.BORDER);		
+        labelOldPassword.setText("Current password:");
+        textOldPassword = new Text(control, SWT.BORDER);
+        textOldPassword.setToolTipText("Please insert your current password.");
         data = new GridData(GridData.FILL_HORIZONTAL);
         textOldPassword.setLayoutData(data);
         
         //password
         labelPassword = new Label(control,SWT.NONE);
-        labelPassword.setText("Please insert your new password:");
-        textPassword = new Text(control, SWT.BORDER);		
+        labelPassword.setText("New password:");
+        textPassword = new Text(control, SWT.BORDER);	
+        textPassword.setToolTipText("Please insert here your new password.");
+        textPassword.addModifyListener( new ModifyListener(){
+            public void modifyText(ModifyEvent e) {
+               UIChangePassword.this.changePassword();
+            }            
+        });
         data = new GridData(GridData.FILL_HORIZONTAL);
         textPassword.setLayoutData(data);
         
         //confirmpassword
         labelConfPass = new Label(control,SWT.NONE);
-        labelConfPass.setText("Please confirm your password:");
-        textConfPass = new Text(control, SWT.BORDER);		
+        labelConfPass.setText("New password:");
+        textConfPass = new Text(control, SWT.BORDER);
+        textConfPass.setToolTipText("Please confirm here your password.");
+        textConfPass.addModifyListener( new ModifyListener(){
+            public void modifyText(ModifyEvent e) {
+               UIChangePassword.this.changePassword();
+            }            
+        });
         data = new GridData(GridData.FILL_HORIZONTAL);
         textConfPass.setLayoutData(data);
         
         return control;
     }
     
+    /**
+     * 
+     */
+    protected void changePassword() {
+        if ( textPassword.getText().equals(textConfPass.getText())){
+            this.setMessage("Passwords are not equal!", IMessageProvider.ERROR);
+            this.okButton.setEnabled(false);
+        } else {
+            this.setMessage(null, IMessageProvider.NONE);
+            this.okButton.setEnabled(true);
+        }
+    }
+    
+    protected void createButtonsForButtonBar(Composite parent) {
+        okButton = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+        createButton(parent, IDialogConstants.CANCEL_ID, 
+                IDialogConstants.CANCEL_LABEL, false); //$NON-NLS-1$
+    }
+
+
+
     public void okPressed(){
         Useractions acts = new Useractions();
         
