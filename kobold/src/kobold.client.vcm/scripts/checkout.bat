74791013440e3729b7e079f2b92a64c555b7e3fe
rendgeor <rendgeor>
2004-08-31 10:26:55 +0000

    changed scripts: now checkout and update need a tag or "HEAD" as default value!

--- kobold/src/kobold.client.vcm/scripts/checkout.bat
+++ kobold/src/kobold.client.vcm/scripts/checkout.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %1
-cvs.exe -z3 -d:pserver:%2:%3@%4:%5 co %6
+cvs.exe -z3 -d:pserver:%2:%3@%4:%5 co -r %7 %6
 
--- kobold/src/kobold.client.vcm/scripts/update.sh
+++ kobold/src/kobold.client.vcm/scripts/update.sh
@@ -12,8 +12,9 @@ cd $1
 
 #$4 cvs repository server: cvs.berlios.de
 #$5 cvs repository path: /cvsroot/kobold
+#$6 tag
 
 #LOGIN wird vorrausgestzt-->erzeugt .cvspass
 #cvs -d :pserver:anonymous@cvs.berlios.de:/cvsroot/kobold login 
-cvs -z3 -d :pserver:$2:$3@$4:$5 up -dPA
+cvs -z3 -d :pserver:$2:$3@$4:$5 up -dPA -r $6
 
