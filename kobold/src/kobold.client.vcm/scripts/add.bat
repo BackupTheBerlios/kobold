6ebca59579f6fe3f1e96fbeb6846ad10a74401e9
grosseml <grosseml>
2004-09-22 16:04:26 +0000

    fixed the syntax error

--- kobold/src/kobold.client.vcm/scripts/add.bat
+++ kobold/src/kobold.client.vcm/scripts/add.bat
@@ -1,7 +1,7 @@
 @echo off
 rem %1 working directory
 rem %2 repo type
-rem %3 protocoal type
+rem %3 protocol type
 rem %4 username
 rem %5 password
 rem %6 host
@@ -11,11 +11,12 @@ rem %9 userdef
 
 cd /d %1
 
-if %2 == CVS goto CVS endif
+IF %2==CVS GOTO CVS
+
 goto END
 
 :CVS
-if %3 == local goto LOCAL endif
+if %3==local goto LOCAL endif
 
 cvs -z3 -d :%3:%4:%5@%6:%7 add %8
 
