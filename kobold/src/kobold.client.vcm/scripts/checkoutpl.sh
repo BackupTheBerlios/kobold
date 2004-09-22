86841e8035fe9217d77935d6889cec242cf2f708
garbeam <garbeam>
2004-09-22 14:09:14 +0000

    new pl checkout scripts

---
+++ kobold/src/kobold.client.vcm/scripts/checkoutpl.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# $1 working directory
+# $2 repo type
+# $3 protocoal type
+# $4 username
+# $5 password
+# $6 host
+# $7 root
+# $8 module
+# $9 userdef
+#
+
+if [ $2 != "CVS" ] ; then
+    exit;
+fi
+
+cd $1
+
+if [ $3 = "local" ] ; then
+
+    cvs -z3 -d $7 co -d . $8
+
+else
+
+    cvs -z3 -d :$3:$4:$5@$6:$7 co -d . $8
+
+fi
