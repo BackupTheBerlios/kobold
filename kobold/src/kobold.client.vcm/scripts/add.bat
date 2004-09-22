a7fd6db44dbadcf8767d4b44150d55f176140a59
grosseml <grosseml>
2004-09-22 16:06:27 +0000

    *** empty log message ***

--- kobold/src/kobold.client.vcm/scripts/add.bat
+++ kobold/src/kobold.client.vcm/scripts/add.bat
@@ -9,20 +9,20 @@ rem %7 root
 rem %8 module
 rem %9 userdef
 
-cd /d %1
+echo cd /d %1
 
 IF %2==CVS GOTO CVS
 
 goto END
 
 :CVS
-if %3==local goto LOCAL endif
+if %3==local goto LOCAL
 
-cvs -z3 -d :%3:%4:%5@%6:%7 add %8
+echo cvs -z3 -d :%3:%4:%5@%6:%7 add %8
 
 goto END
 
 :LOCAL
-cvs -z3 -d %7 add %8
+echo cvs -z3 -d %7 add %8
 
 :END
